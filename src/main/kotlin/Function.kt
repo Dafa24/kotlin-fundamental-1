bd93a6e8da0e2d1f85344216fba1c8c8b6626eda
Dafa24 <akbarpradipa@gmail.com>
2020-08-06 20:13:20 +0700

    initial commit

---
+++ src/main/kotlin/Function.kt
@@ -0,0 +1,5 @@
+fun main (){
+    val user = setUser("Dafa", 16)
+    println(user)
+}
+fun setUser (name:String, age: Int) = "My name is $name, I Like Cycling, and iam $age years old"
---
+++ src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val name = "Dafa"
+    val hobby = "Cycling"
+
+    print("Hello my Name is ")
+    println(name)
+    print("My hobby is ")
+    println(hobby)
+}
